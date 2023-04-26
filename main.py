dbd9ad1a290bcbc90ec770f39c4dc3a292aaac87
Mariusz Lepkowski <mariusz.lepkowski23@gmail.com>
2023-04-26 17:19:38 +0200

    Complete the Application's User Interface

--- main.py
+++ main.py
@@ -25,7 +25,19 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(100))
+check_marks.grid(column=1, row=3)
 
 
 window.mainloop()
